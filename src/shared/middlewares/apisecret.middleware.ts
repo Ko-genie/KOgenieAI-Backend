fed5e6530ace1cb4d3a52dcdeec58959a3c42962
safayet2218 <safayet2218@gmail.com>
2023-09-13 19:06:29 +0600

    update api secret middleware

--- src/shared/middlewares/apisecret.middleware.ts
+++ src/shared/middlewares/apisecret.middleware.ts
@@ -6,9 +6,16 @@ export class ApiSecretCheckMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const appUrl = req.originalUrl;
     const arr = appUrl.split('/');
+    
     if (arr[1] && arr[1] == `${coreConstant.FILE_DESTINATION}`) {
       next();
     } else {
+      const apiSecretKey = req.headers['apisecretkeycheck'];
+
+      if (!apiSecretKey) {
+        throw new BadRequestException('API secret key is required!');
+      }
+
       if (req.headers['apisecretkeycheck'] !== process.env.API_SECRET) {
         throw new BadRequestException('invalid secret key');
       }
