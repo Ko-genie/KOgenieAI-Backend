034388e11ec9284549942d41c6b89f5500aa23a7
safayet2218 <safayet2218@gmail.com>
2023-10-05 16:53:09 +0600

    update

--- src/modules/public/public.module.ts
+++ src/modules/public/public.module.ts
@@ -4,11 +4,17 @@ import { PublicController } from './public.controller';
 import { PublicService } from './public.service';
 import { TrustedOrganizationService } from '../admin/trusted-organization/trusted.service';
 import { SettingService } from '../admin/settings/settings.service';
+import { NotificationService } from 'src/shared/notification/notification.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [PublicController],
-  providers: [PublicService, TrustedOrganizationService, SettingService],
+  providers: [
+    PublicService,
+    TrustedOrganizationService,
+    SettingService,
+    NotificationService,
+  ],
   exports: [PublicService],
 })
 export class PublicModule {}
