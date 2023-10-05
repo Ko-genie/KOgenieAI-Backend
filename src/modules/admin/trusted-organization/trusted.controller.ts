3fe0e2aa302dd41ba8744deac8c8325cbb9b4741
safayet2218 <safayet2218@gmail.com>
2023-10-05 16:06:55 +0600

    update trusted organization

--- src/modules/admin/trusted-organization/trusted.controller.ts
+++ src/modules/admin/trusted-organization/trusted.controller.ts
@@ -1,6 +1,45 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { TrustedOrganizationService } from './trusted.service';
+import { AddNewTrustedOrganizationDto } from './dto/add-new-trusted-organization.dto';
+import { UpdateTrustedOrganizationDto } from './dto/update-trusted-organization.dto';
 
-@Controller()
+@Controller('admin-trusted-organization')
 export class TrustedOrganizationController {
-    
+  constructor(
+    private readonly trustedOrganizationService: TrustedOrganizationService,
+  ) {}
+
+  @Post('add-new')
+  addNewTrustedOrganization(@Body() payload: AddNewTrustedOrganizationDto) {
+    return this.trustedOrganizationService.addNewTrustedOrganization(payload);
+  }
+
+  @Get('list')
+  getListOfTrustedOrganization(@Query() payload: any) {
+    return this.trustedOrganizationService.getListOfTrustedOrganization(
+      payload,
+    );
+  }
+
+  @Get('details-:id')
+  getTrustedOrganizationDetails(@Param('id') id: number) {
+    return this.trustedOrganizationService.getTrustedOrganizationDetails(id);
+  }
+
+  @Post('update')
+  updateTrustedOrganization(@Body() payload: UpdateTrustedOrganizationDto) {
+    return this.trustedOrganizationService.updateTrustedOrganization(payload);
+  }
+  @Delete('delete-:id')
+  deleteTrustedOrganization(@Param('id') id: number) {
+    return this.trustedOrganizationService.deleteTrustedOrganization(id);
+  }
 }
