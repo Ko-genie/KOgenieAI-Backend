82fecce5e937b4fc01b944093a561d96b94eb170
anirban_itech <anirban00537@gmail.com>
2023-10-06 11:34:55 +0600

    updated

--- src/modules/admin/settings/dto/update-payment-stripe-settings.dt.ts
+++ src/modules/admin/settings/dto/update-payment-stripe-settings.dt.ts
@@ -3,23 +3,6 @@ import { ModeStatusArray } from 'src/shared/constants/array.constants';
 
 export class UpdatePaymentMethodStripeSettingsDto {
   @IsNotEmpty()
-  @IsNumber()
-  @IsIn(ModeStatusArray)
-  pm_stripe_status_mode: number;
-
-  @IsOptional()
-  @IsString()
-  pm_stripe_default_currency: string;
-
-  @IsNotEmpty()
-  @IsString()
-  pm_stripe_client_id_sandbox: string;
-
-  @IsNotEmpty()
-  @IsString()
-  pm_stripe_secret_key_sandbox: string;
-
-  @IsNotEmpty()
   @IsString()
   pm_stripe_client_id_live: string;
 
